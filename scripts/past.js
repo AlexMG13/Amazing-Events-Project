937c8677729b21ffcd576765b38727ef8d17c5b8
Alex <mendozaalex24@gmail.com>
2023-02-23 22:17:22 -0300

    display cards in upcoming and past with DOM

--- scripts/past.js
+++ scripts/past.js
@@ -1,22 +1,36 @@
 
 
-function pastDate(dateArr) {
-    let currentDate = data.currentDate;
+function pastDate(arrData,date) {
+    let currentDate = arrData.currentDate;
     let year = parseInt(currentDate.substr(0,4));
-    let month = parseInt(currentDate.substr(5,2));
-    let day = parseInt(currentDate.substr(8,2));
-
-    let yearEvent = parseInt(dateArr.substr(0,4));
-    let monthEvent = parseInt(dateArr.substr(5,2));
-    let dayEvent = parseInt(dateArr.substr(8,2));
-
+    let yearEvent = parseInt(date.substr(0,4));
     if (yearEvent < year){
         return true;
     }
     return false;
 }
 
+const pastEvents = data.events.filter(event => pastDate(data,event.date));
 
-const pastEvents = data.events.filter(event => pastDate(event.date));
+let pastCards = document.getElementById('past-card');
 
-console.log(pastEvents);
+pastCards.innerHTML = paCards(pastEvents);
+
+function paCards(arrData){
+    let cards = '';
+    for (event of arrData){
+        cards += `<div class="card m-2 bg-section-1" style="width: 18rem;">
+        <img src="${event.image}" class="card-img-top mt-2"
+            alt="${event.name}">
+        <div class="card-body text-card-d">
+            <h5 class="card-title">${event.name}</h5>
+            <p class="card-text">${event.description}</p>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <p>Precio: ${event.price}</p>
+                <a href="./details.html" class="btn-b">view more</a>
+            </div>
+        </div>
+    </div>`
+    }
+    return cards;
+}
--- scripts/upcoming.js
+++ scripts/upcoming.js
@@ -1,19 +1,40 @@
 
-function upcomingDate(dateArr) {
-    let currentDate = data.currentDate;
+function upcomingDate(arrData, date) {
+    let currentDate = arrData.currentDate;
     let year = parseInt(currentDate.substr(0,4));
     let month = parseInt(currentDate.substr(5,2));
-    let day = parseInt(currentDate.substr(8,2));
 
-    let yearEvent = parseInt(dateArr.substr(0,4));
-    let monthEvent = parseInt(dateArr.substr(5,2));
-    let dayEvent = parseInt(dateArr.substr(8,2));
+    let yearEvent = parseInt(date.substr(0,4));
+    let monthEvent = parseInt(date.substr(5,2));
 
-    if (yearEvent >= year && monthEvent >= month && dayEvent >= day){
+    if (yearEvent >= year && monthEvent >= month){
         return true;
     }
     return false;
 }
 
+const upcomingEvents = data.events.filter(event => upcomingDate(data,event.date));
 
-const upcomingEvents = data.events.filter(event => upcomingDate(event.date));
+
+let upcomingCards = document.getElementById('upcoming-card');
+
+upcomingCards.innerHTML = upCards(upcomingEvents);
+
+function upCards(arrData){
+    let cards = '';
+    for (event of arrData){
+        cards += `<div class="card m-2 bg-section-1" style="width: 18rem;">
+        <img src="${event.image}" class="card-img-top mt-2"
+            alt="${event.name}">
+        <div class="card-body text-card-d">
+            <h5 class="card-title">${event.name}</h5>
+            <p class="card-text">${event.description}</p>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <p>Precio: ${event.price}</p>
+                <a href="./details.html" class="btn-b">view more</a>
+            </div>
+        </div>
+    </div>`
+    }
+    return cards;
+}
